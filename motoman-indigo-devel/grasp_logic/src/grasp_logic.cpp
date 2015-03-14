826de9c0e50b1e7b693ef3d7e028e7bffccbf240
Kevin Yang <kyang@wpi.edu>
2015-03-14 18:36:27 -0400

    Setting up framework for service node

--- motoman-indigo-devel/grasp_logic/src/grasp_logic.cpp
+++ motoman-indigo-devel/grasp_logic/src/grasp_logic.cpp
@@ -10,22 +10,25 @@ bool service_cb(grasp_logic::grasp::Request  &req,
  {
     geometry_msgs::PoseStamped obj_pose;
     Eigen::Affine3d desired_pose;
-/*
+
     // Stamped Pose msg
-    obj_pose.header.frame_id = "";
+    obj_pose.header.frame_id = req.obj_pose.header.frame_id;
     obj_pose.header.stamp = ros::Time::now();
 
-    obj_pose.pose.position.x = 0.94;
-    obj_pose.pose.position.y = -0.31;
-    obj_pose.pose.position.z = 0.13;
-    obj_pose.pose.orientation.x = 0.3;
-    obj_pose.pose.orientation.y = 0.7;
-    obj_pose.pose.orientation.z = -0.3;
-    obj_pose.pose.orientation.w = 0.7;
+    obj_pose.pose.position.x = req.obj_pose.pose.position.x;
+    obj_pose.pose.position.y = req.obj_pose.pose.position.y;
+    obj_pose.pose.position.z = req.obj_pose.pose.position.z;
+    obj_pose.pose.orientation.x = req.obj_pose.pose.orientation.x;
+    obj_pose.pose.orientation.y = req.obj_pose.pose.orientation.y;
+    obj_pose.pose.orientation.z = req.obj_pose.pose.orientation.z;
+    obj_pose.pose.orientation.w = req.obj_pose.pose.orientation.w;
     Eigen::Translation3d translation(obj_pose.pose.position.x, obj_pose.pose.position.y, obj_pose.pose.position.z);
     Eigen::Quaterniond rotation(obj_pose.pose.orientation.w, obj_pose.pose.orientation.x, obj_pose.pose.orientation.y, obj_pose.pose.orientation.z);
     Eigen::Affine3d target_pose = translation * rotation;
-*/
+
+
+    res.arm_pose = obj_pose;
+
     return true;
  }
 
--- motoman-indigo-devel/grasp_logic/src/grasp_logic_client.cpp
+++ motoman-indigo-devel/grasp_logic/src/grasp_logic_client.cpp
@@ -5,35 +5,6 @@
 
 #include "grasp_logic/grasp.h"
 
-bool service_cb(grasp_logic::grasp::Request  &req,
-         grasp_logic::grasp::Response &res)
- {
-    geometry_msgs::PoseStamped obj_pose;
-    Eigen::Affine3d desired_pose;
-
-    std::string obj_name = req.object;
-
-    // copy posed stamped msg from requestor
-    obj_pose.header.frame_id = req.obj_pose.header.frame_id;
-    obj_pose.header.stamp = ros::Time::now();
-    obj_pose.pose.position.x = req.obj_pose.pose.position.x;
-    obj_pose.pose.position.y = req.obj_pose.pose.position.y;
-    obj_pose.pose.position.z = req.obj_pose.pose.position.z;
-    obj_pose.pose.orientation.x = req.obj_pose.pose.orientation.x;
-    obj_pose.pose.orientation.y = req.obj_pose.pose.orientation.y;
-    obj_pose.pose.orientation.z = req.obj_pose.pose.orientation.z;
-    obj_pose.pose.orientation.w = req.obj_pose.pose.orientation.w;
-    Eigen::Translation3d translation(obj_pose.pose.position.x, obj_pose.pose.position.y, obj_pose.pose.position.z);
-    Eigen::Quaterniond rotation(obj_pose.pose.orientation.w, obj_pose.pose.orientation.x, obj_pose.pose.orientation.y, obj_pose.pose.orientation.z);
-    Eigen::Affine3d current_pose = translation * rotation;
-
-
-
-
-    return true;
- }
-
-
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "grasp_logic_client");
@@ -42,7 +13,7 @@ int main(int argc, char** argv)
     ros::ServiceClient client = nh.serviceClient<grasp_logic::grasp>("grasp_logic");
     grasp_logic::grasp srv;
     geometry_msgs::PoseStamped EE_pose;
-/*
+
 
     // Stamped Pose
     EE_pose.header.frame_id = "camera_frame";
@@ -58,18 +29,20 @@ int main(int argc, char** argv)
     Eigen::Quaterniond rotation(EE_pose.pose.orientation.w, EE_pose.pose.orientation.x, EE_pose.pose.orientation.y, EE_pose.pose.orientation.z);
     Eigen::Affine3d target_pose = translation * rotation;
 
-    srv.request.obj_pose = EE_pose;
+    //srv.request.obj_pose = EE_pose;
     srv.request.object = "Crayons";
+    srv.request.obj_pose = EE_pose;
 
     if (client.call(srv))
      {
-       ROS_INFO("Target Arm Pose", (long int)srv.response.arm_pose);
+       //ROS_INFO("Target Arm Pose", srv.response.arm_pose);
+        std::cout << srv.response.arm_pose << std::endl;
      }
      else
      {
        ROS_ERROR("Failed to call service grasp_logic");
        return 1;
-*/
+     }
     ros::spin();
 
     return 0;
