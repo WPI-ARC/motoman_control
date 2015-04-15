90f5c7664417b8b5ecaba23ea6baf1a3b09c7e17
Alex Henning <alex@thoriumrobotics.com>
2015-04-15 15:59:11 -0400

    Updated control to support shelf collision avoidance

--- task_controller/nodes/util.py
+++ task_controller/nodes/util.py
@@ -1,28 +1,77 @@
 
 import rospy
 
-from geometry_msgs.msg import PoseStamped
+import moveit_commander
 
-def goto_pose(group, pose, times=[5, 20, 40, 60]):
+from geometry_msgs.msg import PoseStamped
+from moveit_msgs.msg import CollisionObject
+
+scene = moveit_commander.PlanningSceneInterface()
+
+
+def goto_pose(group, pose, times=[5, 20, 40, 60], add_shelf=True):
+    """Moves the hand to a given `pose`, using the configured `group`. The
+    planning time is modified based on the passed in `times` to try to
+    plan quickly if possible, but fall back on longer plans if
+    necessary. If `add_shelf` is true, a box model of the shelf is
+    added to the environment to avoid collisions."""
+    if add_shelf:
+        add_shelf()
     for t in times:
         group.set_planning_time(t)
         rospy.loginfo("Planning for "+str(t)+" seconds...")
         result = group.go(pose)
         if result:
+            if add_shelf:
+                remove_shelf()
             return True
+    if add_shelf:
+        remove_shelf()
     return False
 
+
 def follow_path(group, path, collision_checking=True):
+    """Follows a cartesian path using a linear interpolation of the given
+    `path`. The `collision_checking` parameter controls whether or not
+    to check the path for collisions with the environment."""
     traj, success = group.compute_cartesian_path(
         path,
-        0.01, # 1cm interpolation resolution
-        0.0, # jump_threshold disabled
-        avoid_collisions = collision_checking,
+        0.01,  # 1cm interpolation resolution
+        0.0,  # jump_threshold disabled
+        avoid_collisions=collision_checking,
     )
     if success < 1:
         rospy.logwarn("Cartesian trajectory could not be completed. Only solved for: '"+str(success)+"'...")
         return False
     return group.execute(traj)
+
+
+def add_shelf():
+    pose = PoseStamped()
+    pose.header.frame_id = "/base_link"
+    pose.header.stamp = rospy.Time.now()
+    pose.pose.position.x = 1.28
+    pose.pose.position.y = 0
+    pose.pose.position.z = 1.25
+    pose.pose.orientation.x = 0.5
+    pose.pose.orientation.y = 0.5
+    pose.pose.orientation.z = 0.5
+    pose.pose.orientation.w = 0.5
+    scene.add_box(
+        name="shelf",
+        pose=pose,
+        size=(1, 2.5, 1)
+    )
+
+
+def remove_shelf():
+    co = CollisionObject()
+    co.operation = CollisionObject.Remove
+    co.id = "shelf"
+    co.header.frame_id = "/base_link"
+    co.header.stamp = rospy.Time.now()
+    scene._pub_co.publish(co)
+
 
 def bin_pose(bin, bin_x=1.32, bin_y=0, bin_z=-0.01):
     # Setting Configuration:
