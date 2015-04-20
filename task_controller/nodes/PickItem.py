456c21977a57be7b3436abe6642e26410788be0b
Alex Henning <alex@thoriumrobotics.com>
2015-04-20 18:27:38 -0400

    Replaced picking with Kevin's database

--- task_controller/nodes/PickItem.py
+++ task_controller/nodes/PickItem.py
@@ -1,12 +1,13 @@
 import roslib; roslib.load_manifest('task_controller')
 import rospy
 import smach
+import tf2_ros
+from geometry_msgs.msg import TransformStamped
 
-from copy import deepcopy
-from grasp_logic.srv import grasp, graspRequest
-from gripper_srv.srv import gripper, gripperRequest
+from gripper_srv.srv import gripper
+from grasp_planner.srv import apcGraspDB
 
-from util import follow_path, bin_pose
+from util import filterGrasps, execute_grasp
 
 
 class PICKITEM(smach.State):
@@ -16,56 +17,54 @@ class PICKITEM(smach.State):
                              input_keys=['input', 'item', 'pose', 'bin'],
                              output_keys=['output'])
         self.arm = robot.arm_left
-        self.grasp_generator = rospy.ServiceProxy("grasp_logic", grasp)
+        self.grasp_generator = rospy.ServiceProxy('getGrasps', apcGraspDB)
         self.gripper_control = rospy.ServiceProxy("/left/command_gripper", gripper)
 
-        #request = gripperRequest(command="activate")
         # TODO: Handle response error
-        #response = self.gripper_control.call(request)
-        #print "Activate Gripper:", response
+        response = self.gripper_control.call(command="activate")
+        print "Activate Gripper:", response
 
     def execute(self, userdata):
         rospy.loginfo("Trying to pick '"+userdata.item+"'...")
         userdata.output = userdata.input
 
-        request = graspRequest(
-            object=userdata.item,
-            obj_pose=userdata.pose
-        )
         # TODO: Handle response error
-        response = self.grasp_generator.call(request)
-        pose = response.arm_pose.pose
-        #pose.orientation = self.arm.get_current_pose().pose.orientation
-        print "Grasp Pose:", response
+        response = self.grasp_generator.call(item=userdata.item,
+                                             Trob_obj=userdata.pose)
+        grasps = filterGrasps(self.arm, response.apcGraspArray.grasps)
+        print "Grasp:", grasps[0]
+
+        tfs = []
+        for i in range(len(grasps)):
+            grasp = grasps[i].posegrasp
+            approach = grasps[i].poseapproach
+            t = TransformStamped()
+            t.header.stamp = rospy.Time.now()
+            t.header.frame_id = "base_link"
+            t.child_frame_id = "grasp "+str(i)
+            t.transform.translation = grasp.position
+            t.transform.rotation = grasp.orientation
+            tfs.append(t)
+            t = TransformStamped()
+            t.header.stamp = rospy.Time.now()
+            t.header.frame_id = "base_link"
+            t.child_frame_id = "approach "+str(i)
+            t.transform.translation = approach.position
+            t.transform.rotation = approach.orientation
+            tfs.append(t)
+
+        br = tf2_ros.TransformBroadcaster()
+        rate = rospy.Rate(250.0)
+        while (not rospy.is_shutdown()):
+            for t in tfs:
+                t.header.stamp = rospy.Time.now()
+                br.sendTransform(t)
+            rate.sleep()
 
         self.arm.set_planner_id("RRTstarkConfigDefault")
         self.arm.set_workspace([-3, -3, -3, 3, 3, 3])
 
-        print "Moving to grasp pose"
-        out = self.arm.get_current_pose().pose
-        up = deepcopy(out)
-        up.position.z += 0.03  # 3cm
-        target = deepcopy(pose)
-        target.position.x = up.position.x + 0.3  # 2cm
-        target.position.z = up.position.z - 0.02  # 2cm
-        if not follow_path(self.arm, [out, up, target]):
-            return 'Failure'
-
-        request = gripperRequest(command="close")
-        # TODO: Handle response error
-        response = self.gripper_control.call(request)
-        print "Close Gripper:", response
-
-        rospy.sleep(rospy.Duration(1, 0)) # TODO: Shouldn't need
-
-        print "Retreat from grasp"
-        lift = deepcopy(self.arm.get_current_pose().pose)
-        lift.position.z += 0.03
-        up.position.x -= 0.15
-        up.position.z += 0.02
-        if not follow_path(self.arm, [self.arm.get_current_pose().pose, lift, up]):
-            return 'Failure'
-
-        rospy.sleep(rospy.Duration(1, 0)) # TODO: Shouldn't need
+        if not execute_grasp(self.arm, grasps[0], userdata.pose):
+            return "Failure"
 
         return 'Success'
