586af45ebe17188c4338f65528fa0313bf3b4e73
wpi-apc <wpi-apc@wpiapc.(none)>
2015-04-21 16:26:42 -0400

    Can pick up tray
    Can pick up tray with left hand from bin E,
    we also now update the model of the hands in RVIZ when actuating them

--- task_controller/nodes/PickScoop.py
+++ task_controller/nodes/PickScoop.py
@@ -2,7 +2,7 @@ import roslib; roslib.load_manifest('task_controller')
 import rospy
 import smach
 import tf2_ros
-from geometry_msgs.msg import TransformStamped, Pose
+from geometry_msgs.msg import TransformStamped, Pose, Quaternion
 
 from gripper_srv.srv import gripper
 from motoman_moveit.srv import convert_trajectory_server
@@ -29,44 +29,58 @@ class PICKSCOOP(smach.State):
         rospy.loginfo("Trying to pick scoop...")
         userdata.output = userdata.input
 
-        print self.gripper_control(command="open")
+        print self.gripper_control(command="70")
 
-        targetPose = Pose()
-        targetPose.position.x = 0.735592
-        targetPose.position.z = 0.446805
-        targetPose.position.y = 1.18608
-        targetPose.orientation.x = -0.108456
-        targetPose.orientation.y = 0.708157
-        targetPose.orientation.z = 0.693177
-        targetPose.orientation.w = 0.0790984
-        targetPose.position.x = 0.509;
-        targetPose.position.y = 0.873;
-        targetPose.position.z = 1.25832;
-        targetPose.orientation.x = -0.673667;
-        targetPose.orientation.y = 0.207501;
-        targetPose.orientation.z = 0.706735;
-        targetPose.orientation.w = -0.0603374;
+        orientation = Quaternion(x=-0.083657, y=0.71336, z=0.69142, w=0.077827)
+        initial = Pose(orientation=orientation)
+        approach = Pose(orientation=orientation)
+        target = Pose(orientation=orientation)
+        initial.position.x = 0.49221
+        initial.position.y = 0.4087
+        initial.position.z = 1.15
+        approach.position.x = 0.77
+        approach.position.y = 0.40794
+        approach.position.z = 1.15
+        target.position.x = 0.77
+        target.position.y = 0.25672
+        target.position.z = 1.17
 
         self.arm.set_planner_id("RRTstarkConfigDefault")
         self.arm.set_workspace([-3, -3, -3, 3, 3, 3])
         self.arm.set_planning_time(1)
-        plan = self.arm.plan(targetPose)
-        print plan
+        plan = self.arm.plan(initial)
+        #print plan
         print self.move(plan.joint_trajectory)
 
-        print self.gripper_control(command="70")
-
-        poses = [self.arm.get_current_pose().pose]
-        poses.append(deepcopy(poses[-1]))
-        poses[-1].position.x += 0.206
-        poses[-1].position.z += -0.048
+        poses = [self.arm.get_current_pose().pose, approach, target]
         traj, success = self.arm.compute_cartesian_path(
             poses,
             0.01,  # 1cm interpolation resolution
             0.0,  # jump_threshold disabled
             avoid_collisions=True,
         )
-        print traj
+        #print traj
+        if success < 1:
+            rospy.logwarn("Cartesian trajectory could not be completed. Only solved for: '"+str(success)+"'...")
+
+        raw_input("Continue?")
+        print self.move(traj.joint_trajectory)
+        print self.gripper_control(command="close")
+
+
+        raw_input("Continue?")
+        print "Picking scoop"
+        poses = [self.arm.get_current_pose().pose]
+        poses.append(deepcopy(poses[-1]))
+        poses[-1].position.x -= 0.5
+        poses[-1].position.z += 0.25
+        traj, success = self.arm.compute_cartesian_path(
+            poses,
+            0.01,  # 1cm interpolation resolution
+            0.0,  # jump_threshold disabled
+            avoid_collisions=True,
+        )
+        #print traj
         if success < 1:
             rospy.logwarn("Cartesian trajectory could not be completed. Only solved for: '"+str(success)+"'...")
 
