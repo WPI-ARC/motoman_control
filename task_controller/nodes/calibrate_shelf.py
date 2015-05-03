bb1ece5a57ca24f8737786b48630b2ac322db3d1
Alex Henning <alex@thoriumrobotics.com>
2015-05-03 15:46:13 -0400

    Updated calibration code to move to initial pose

--- task_controller/nodes/calibrate_shelf.py
+++ task_controller/nodes/calibrate_shelf.py
@@ -5,27 +5,37 @@ import rospy
 import subprocess
 import moveit_commander
 
+from geometry_msgs.msg import Pose, Point, Quaternion
+
 depth = 0.87  # Depth of shelf
 palm_offset = 0.165  # Offset from tool to palm
 
-#   orientation:
-#     x: 0.557211015648
-#     y: -0.438833281977
-#     z: -0.561100598534
-#     w: 0.426740381256
 
-# arm_left
-# pose:
-#   position:
-#     x: 0.539323677836
-#     y: 0.433382331813
-#     z: 1.6160210999
-# arm_right
-# pose:
-#   position:
-#     x: 0.526801535149
-#     y: -0.379182889409
-#     z: 0.88944822383
+default_orientation = Quaternion(
+    x=0.557211015648,
+    y=-0.438833281977,
+    z=-0.561100598534,
+    w=0.426740381256,
+)
+
+pose_left = Pose(
+    position=Point(
+        x=0.539323677836,
+        y=0.433382331813,
+        z=1.6160210999,
+    ),
+    orientation=default_orientation,
+)
+
+pose_right = Pose(
+    position=Point(
+        x=0.526801535149,
+        y=-0.379182889409,
+        z=0.88944822383,
+    ),
+    orientation=default_orientation,
+)
+
 
 if __name__ == '__main__':
     path = subprocess.check_output("rospack find task_controller", shell=True)
@@ -35,8 +45,10 @@ if __name__ == '__main__':
     robot = moveit_commander.RobotCommander()
 
     print "Starting up"
-    raw_input("Hit enter to move to initial pose ")
-    # TODO: Move to initial pose
+    skip = raw_input("Hit enter to move to initial pose or type skip to skip: ")
+    if "s" not in skip:
+        print "Left: ", robot.arm_left.go(pose_left)
+        print "Right: ", robot.arm_right.go(pose_right)
     print "Please jog until the palms touch the shelf"
     raw_input("Hit enter to continue ")
     left = robot.arm_left.get_current_pose().pose
