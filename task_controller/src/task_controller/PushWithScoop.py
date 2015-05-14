6d67d87f7f0694e5f86003e81f07b8c226205f6c
Ted Armstrong <theodorecarmstrong@gmail.com>
2015-05-13 20:33:31 -0400

    trivial changes to pushwithscoop

--- task_controller/src/task_controller/PushWithScoop.py
+++ task_controller/src/task_controller/PushWithScoop.py
@@ -34,8 +34,8 @@ class PushWithScoop( smach.State ):
 
 
         with bin_pose(userdata.bin).pose as initial_bin_pose:
-            self.arm.set_planner_id( "RRTstarkConfigDefault" )
-            self.arm.set_workspace( [-3, -3, -3, 3, 3, 3] )
+            # self.arm.set_planner_id( "RRTstarkConfigDefault" )
+            # self.arm.set_workspace( [-3, -3, -3, 3, 3, 3] )
             self.arm.set_pose_reference_frame( "/shelf" )
             self.arm.set_goal_orientation_tolerance( 0.1 )
             self.arm.set_goal_position_tolerance( 0.005 )
@@ -44,20 +44,20 @@ class PushWithScoop( smach.State ):
 
 #*   This code is soon to be deprecated
             # Get current pose, and convert to shelf frame
-            pose = [ convertFrameRobotToShelf(
+            poses = [ convertFrameRobotToShelf(
                             self.arm.get_current_pose().pose ) ]
 
-            pose.append(deepcopy(pose[-1]))
-            pose[-1] = initial_bin_pose
-            pose[-1].orientation.x =  0.26324
-            pose[-1].orientation.y = -0.66221
-            pose[-1].orientation.z = -0.099005
-            pose[-1].orientation.w =  0.69454
+            poses.append(deepcopy(pose[-1]))
+            poses[-1] = initial_bin_pose
+            poses[-1].orientation.x =  0.26324
+            poses[-1].orientation.y = -0.66221
+            poses[-1].orientation.z = -0.099005
+            poses[-1].orientation.w =  0.69454
             #convert to shelf frame
-            pose[-1] = convertFrameRobotToShelf( pose[-1] )
+            poses[-1] = convertFrameRobotToShelf( pose[-1] )
 
             #move in front of the bin
-            if not follow_path(self.arm, pose):
+            if not goto_pose(self.arm, poses):
                 return 'Failure'
 
 #* end of code deprecation
@@ -76,7 +76,31 @@ class PushWithScoop( smach.State ):
             poses[-1].position.x += 0.25
             poses[-1].position.y += 0.05
 
-            if not follow_path(self.arm, pose):
+            rospy.loginfo( "Pushing items to right side" )
+            #adjust orientation?
+            poses.append( deepcopy( poses[-1] ) )
+            poses[-1].position.y -= 0.2
+
+
+            rospy.loginfo( "Removing from bin " )
+            poses.append( deepcopy( poses[-1] ) )
+            poses[-1].position.y += 0.05
+            poses[-1].position.x -= 0.3
+
+
+            if not follow_path(self.arm, poses)
+                return 'Failure'
+
+            poses = [ convertFrameRobotToShelf(
+                                        self.arm.get_current_pose().pose ) ]
+
+            poses.append(deepcopy(pose[-1]))
+            poses[-1] = initial_bin_pose
+            #convert to shelf frame
+            poses[-1] = convertFrameRobotToShelf( pose[-1] )
+
+
+            if not goto_path(self.arm, poses):
                 return 'Failure'
 
             return 'True'
