02c538b3f07cd7f8543be766e9bf3cb0a898658b
Alex Henning <alex@thoriumrobotics.com>
2015-05-15 21:21:47 -0400

    Added updated handle_order code

--- task_controller/nodes/handle_order.py
+++ task_controller/nodes/handle_order.py
@@ -4,13 +4,55 @@ import rospy
 import json
 import os
 
-from task_controller.MotomanController import MotomanController
-from task_controller.Scheduler import APCScheduler
-from apc_msgs.msg import WorkOrder, APCItem
-from apc_util.services import wait_for_services
+from task_controller.srv import SetSchedule
+from apc_msgs.msg import APCItem, ScheduleItem
+
+
+def schedule_from_order(order):
+    single_items = []
+    double_items = []
+    multiple_items = []
+
+    ranks = rospy.get_param("/item_priorities")
+    for item in order:
+        rank = ranks.index(item.name)
+        if len(item.contents) == 1:
+            single_items.append((rank, item))
+        elif len(item.contents) == 2:
+            double_items.append((rank, item))
+        else:
+            multiple_items.append((rank, item))
+
+    single_items.sort()
+    double_items.sort()
+    multiple_items.sort()
+
+    schedule = []
+    for _, element in single_items:
+        schedule.append(ScheduleItem(
+            action="grab",
+            name=element.name,
+            bin=element.bin,
+            contents=element.contents,
+        ))
+    for _, element in double_items:
+        schedule.append(ScheduleItem(
+            action="grab",
+            name=element.name,
+            bin=element.bin,
+            contents=element.contents,
+        ))
+    for _, element in multiple_items:
+        schedule.append(ScheduleItem(
+            action="grab",
+            name=element.name,
+            bin=element.bin,
+            contents=element.contents,
+        ))
+    return schedule
 
 if __name__ == '__main__':
-    rospy.init_node("motoman_apc_controller")
+    rospy.init_node("handle_order")
 
     order_file = rospy.get_param("~order", "default.json")
 
@@ -21,9 +63,9 @@ if __name__ == '__main__':
     with open(order_file) as file:
         raw_order = json.load(file)
 
-    order = WorkOrder()
+    order = []
     for element in raw_order["work_order"]:
-        order.items.append(APCItem(
+        order.append(APCItem(
             key=element["bin"][-1]+"-"+element["item"],  # TODO: validate unique
             name=element["item"],
             bin=element["bin"][-1],
@@ -32,7 +74,12 @@ if __name__ == '__main__':
 
     print order
 
-    wait_for_services()
+    schedule = schedule_from_order(order)
 
-    controller = MotomanController(APCScheduler(order))
-    controller.Start()
+    print schedule
+
+    set_schedule = rospy.ServiceProxy('set_schedule', SetSchedule)
+    rospy.loginfo("Waiting for service `/set_schedule`")
+    set_schedule.wait_for_service()
+    rospy.loginfo("`/set_schedule` available")
+    set_schedule(schedule)
